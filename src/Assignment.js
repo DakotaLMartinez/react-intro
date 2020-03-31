7a6fa5cf5a3245044be41da585b6fd7f27a45980
DakotaLMartinez <dakotaleemusic@gmail.com>
2020-03-31 16:24:04 -0700

    update our components

---
+++ src/Assignment.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Assignment({ details }) {
+  return (
+    <div>
+      <h2>Assignment for Today</h2>
+      <p>{details}</p>
+      
+    </div>
+  )
+}
+
+export default Assignment
